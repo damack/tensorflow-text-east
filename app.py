ad1caf9a71d9ffcd93f26dfef1061278a2cc45e6
Sebastian Netuschil <sebastian.netuschil@sap.com>
2019-01-25 13:42:27 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,231 @@
+import os
+import re
+import cv2
+import numpy as np
+
+import lanms
+import model
+import pytesseract
+import tensorflow as tf
+from PIL import Image, ImageDraw
+from flask import Flask, request, jsonify
+
+graph = tf.Graph()
+with graph.as_default():
+    input_images = tf.placeholder(
+        tf.float32, shape=[None, None, None, 3], name='input_images')
+    global_step = tf.get_variable(
+        'global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+    f_score, f_geometry = model.model(input_images, is_training=False)
+
+    variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
+    saver = tf.train.Saver(variable_averages.variables_to_restore())
+
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    saver.restore(sess, './model/model.ckpt-49491')
+
+
+def resize_image(im, max_side_len=2400):
+    h, w, _ = im.shape
+    resize_w = w
+    resize_h = h
+    if max(resize_h, resize_w) > max_side_len:
+        ratio = float(
+            max_side_len) / resize_h if resize_h > resize_w else float(max_side_len) / resize_w
+    else:
+        ratio = 1.
+    resize_h = int(resize_h * ratio)
+    resize_w = int(resize_w * ratio)
+
+    resize_h = resize_h if resize_h % 32 == 0 else (resize_h // 32 - 1) * 32
+    resize_w = resize_w if resize_w % 32 == 0 else (resize_w // 32 - 1) * 32
+    im = cv2.resize(im, (int(resize_w), int(resize_h)))
+
+    ratio_h = resize_h / float(h)
+    ratio_w = resize_w / float(w)
+
+    return im, (ratio_h, ratio_w)
+
+
+def restore_rectangle(origin, geometry):
+    d = geometry[:, :4]
+    angle = geometry[:, 4]
+    # for angle > 0
+    origin_0 = origin[angle >= 0]
+    d_0 = d[angle >= 0]
+    angle_0 = angle[angle >= 0]
+    if origin_0.shape[0] > 0:
+        p = np.array([np.zeros(d_0.shape[0]), -d_0[:, 0] - d_0[:, 2],
+                      d_0[:, 1] + d_0[:, 3], -d_0[:, 0] - d_0[:, 2],
+                      d_0[:, 1] + d_0[:, 3], np.zeros(d_0.shape[0]),
+                      np.zeros(d_0.shape[0]), np.zeros(d_0.shape[0]),
+                      d_0[:, 3], -d_0[:, 2]])
+        p = p.transpose((1, 0)).reshape((-1, 5, 2))  # N*5*2
+
+        rotate_matrix_x = np.array(
+            [np.cos(angle_0), np.sin(angle_0)]).transpose((1, 0))
+        rotate_matrix_x = np.repeat(
+            rotate_matrix_x, 5, axis=1).reshape(-1, 2, 5).transpose((0, 2, 1))  # N*5*2
+
+        rotate_matrix_y = np.array(
+            [-np.sin(angle_0), np.cos(angle_0)]).transpose((1, 0))
+        rotate_matrix_y = np.repeat(
+            rotate_matrix_y, 5, axis=1).reshape(-1, 2, 5).transpose((0, 2, 1))
+
+        p_rotate_x = np.sum(rotate_matrix_x * p,
+                            axis=2)[:, :, np.newaxis]  # N*5*1
+        p_rotate_y = np.sum(rotate_matrix_y * p,
+                            axis=2)[:, :, np.newaxis]  # N*5*1
+
+        p_rotate = np.concatenate([p_rotate_x, p_rotate_y], axis=2)  # N*5*2
+
+        p3_in_origin = origin_0 - p_rotate[:, 4, :]
+        new_p0 = p_rotate[:, 0, :] + p3_in_origin  # N*2
+        new_p1 = p_rotate[:, 1, :] + p3_in_origin
+        new_p2 = p_rotate[:, 2, :] + p3_in_origin
+        new_p3 = p_rotate[:, 3, :] + p3_in_origin
+
+        new_p_0 = np.concatenate([new_p0[:, np.newaxis, :], new_p1[:, np.newaxis, :],
+                                  new_p2[:, np.newaxis, :], new_p3[:, np.newaxis, :]], axis=1)  # N*4*2
+    else:
+        new_p_0 = np.zeros((0, 4, 2))
+    # for angle < 0
+    origin_1 = origin[angle < 0]
+    d_1 = d[angle < 0]
+    angle_1 = angle[angle < 0]
+    if origin_1.shape[0] > 0:
+        p = np.array([-d_1[:, 1] - d_1[:, 3], -d_1[:, 0] - d_1[:, 2],
+                      np.zeros(d_1.shape[0]), -d_1[:, 0] - d_1[:, 2],
+                      np.zeros(d_1.shape[0]), np.zeros(d_1.shape[0]),
+                      -d_1[:, 1] - d_1[:, 3], np.zeros(d_1.shape[0]),
+                      -d_1[:, 1], -d_1[:, 2]])
+        p = p.transpose((1, 0)).reshape((-1, 5, 2))  # N*5*2
+
+        rotate_matrix_x = np.array(
+            [np.cos(-angle_1), -np.sin(-angle_1)]).transpose((1, 0))
+        rotate_matrix_x = np.repeat(
+            rotate_matrix_x, 5, axis=1).reshape(-1, 2, 5).transpose((0, 2, 1))  # N*5*2
+
+        rotate_matrix_y = np.array(
+            [np.sin(-angle_1), np.cos(-angle_1)]).transpose((1, 0))
+        rotate_matrix_y = np.repeat(
+            rotate_matrix_y, 5, axis=1).reshape(-1, 2, 5).transpose((0, 2, 1))
+
+        p_rotate_x = np.sum(rotate_matrix_x * p,
+                            axis=2)[:, :, np.newaxis]  # N*5*1
+        p_rotate_y = np.sum(rotate_matrix_y * p,
+                            axis=2)[:, :, np.newaxis]  # N*5*1
+
+        p_rotate = np.concatenate([p_rotate_x, p_rotate_y], axis=2)  # N*5*2
+
+        p3_in_origin = origin_1 - p_rotate[:, 4, :]
+        new_p0 = p_rotate[:, 0, :] + p3_in_origin  # N*2
+        new_p1 = p_rotate[:, 1, :] + p3_in_origin
+        new_p2 = p_rotate[:, 2, :] + p3_in_origin
+        new_p3 = p_rotate[:, 3, :] + p3_in_origin
+
+        new_p_1 = np.concatenate([new_p0[:, np.newaxis, :], new_p1[:, np.newaxis, :],
+                                  new_p2[:, np.newaxis, :], new_p3[:, np.newaxis, :]], axis=1)  # N*4*2
+    else:
+        new_p_1 = np.zeros((0, 4, 2))
+    return np.concatenate([new_p_0, new_p_1])
+
+
+def sort_poly(p):
+    min_axis = np.argmin(np.sum(p, axis=1))
+    p = p[[min_axis, (min_axis+1) % 4, (min_axis+2) % 4, (min_axis+3) % 4]]
+    if abs(p[0, 0] - p[1, 0]) > abs(p[0, 1] - p[1, 1]):
+        return p
+    else:
+        return p[[0, 3, 2, 1]]
+
+
+# the webserver
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    import io
+    bio = io.BytesIO()
+    request.files['image'].save(bio)
+    img = cv2.imdecode(np.frombuffer(bio.getvalue(), dtype='uint8'), 1)
+
+    im_resized, (ratio_h, ratio_w) = resize_image(img)
+    with graph.as_default():
+        score_map, geo_map = sess.run([f_score, f_geometry], feed_dict={
+                                      input_images: [im_resized[:, :, ::-1]]})
+
+    if len(score_map.shape) == 4:
+        score_map = score_map[0, :, :, 0]
+        geo_map = geo_map[0, :, :, ]
+
+    xy_text = np.argwhere(score_map > 0.8)
+    xy_text = xy_text[np.argsort(xy_text[:, 0])]
+    text_box_restored = restore_rectangle(
+        xy_text[:, ::-1]*4, geo_map[xy_text[:, 0], xy_text[:, 1], :])
+    boxes = np.zeros((text_box_restored.shape[0], 9), dtype=np.float32)
+    boxes[:, :8] = text_box_restored.reshape((-1, 8))
+    boxes[:, 8] = score_map[xy_text[:, 0], xy_text[:, 1]]
+
+    boxes = lanms.merge_quadrangle_n9(boxes.astype('float32'), 0.2)
+
+    for i, box in enumerate(boxes):
+        mask = np.zeros_like(score_map, dtype=np.uint8)
+        cv2.fillPoly(mask, box[:8].reshape(
+            (-1, 4, 2)).astype(np.int32) // 4, 1)
+        boxes[i, 8] = cv2.mean(score_map, mask)[0]
+
+    if len(boxes) > 0:
+        boxes = boxes[boxes[:, 8] > 0.1]
+        scores = boxes[:, 8].reshape(-1)
+        boxes = boxes[:, :8].reshape((-1, 4, 2))
+        boxes[:, :, 0] /= ratio_w
+        boxes[:, :, 1] /= ratio_h
+
+    text_lines = []
+    if len(boxes) > 0:
+        text_lines = []
+        for box, score in zip(boxes, scores):
+            box = sort_poly(box.astype(np.int32))
+            if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3]-box[0]) < 5:
+                continue
+
+            polygon = []
+            for point in box:
+                polygon.append({
+                    "X": int(point[0]),
+                    "Y": int(point[1])
+                })
+
+            imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            x, y, width, height = cv2.boundingRect(box)
+            pil_img = Image.fromarray(imgray[y:y+height, x:x+width])
+            text = re.sub('\W+', '', pytesseract.image_to_string(pil_img))
+
+            if(len(text) > 0):
+                text_lines.append({
+                    "Confidence": round(100 - (score * 100), 2),
+                    "DetectedText": text,
+                    "Geometry": {
+                        "BoundingBox": {
+                            "Height": int(height),
+                            "Left": int(x),
+                            "Top": int(y),
+                            "Width": int(width)
+                        },
+                        "Polygon": polygon
+                    }
+                })
+    ret = {
+        'TextDetections': text_lines
+    }
+
+    return jsonify(ret)
+
+
+port = 8000
+if os.environ.get('PORT'):
+    port = os.environ.get('PORT')
+
+app.run('0.0.0.0', port)
